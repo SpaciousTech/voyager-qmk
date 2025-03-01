a711b3ef0b9f84441a1f8b715f29dafcaa75efa6
Space <55485515+SpaciousTech@users.noreply.github.com>
2025-03-01 06:53:15 +0000

    deleted keyoverride

--- DqN6J/keymap.c
+++ DqN6J/keymap.c
@@ -348,12 +348,3 @@ tap_dance_action_t tap_dance_actions[] = {
     [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
     [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
 }
-
-
-
-// Custom QMK here
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL); // This is a custom key override that will replace the default behavior of the shift+BSPC key with the DEL key.
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL};

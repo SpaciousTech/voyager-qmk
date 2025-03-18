1e4e87a62a3c22c312b7b236c2502ec41664ef87
Space <55485515+SpaciousTech@users.noreply.github.com>
2025-03-18 01:15:23 +0000

    edited definitions

--- DqN6J/features/select_word.h
+++ DqN6J/features/select_word.h
@@ -14,7 +14,7 @@
 
 /**
  * @file select_word.h
- * @brief Select Word community module: select words and lines.
+ * @brief Select word/line macro.
  *
  * Overview
  * --------
@@ -35,56 +35,92 @@
 #include "quantum.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
+  /**
+   * @brief Select Word keycode.
+   *
+   * In your `keymap.c` file, add a custom keycode (say, `SELWORD`) for activating
+   * the Select Word macro and use the new keycode somewhere in your layout. Then
+   * define `SELECT_WORD_KEYCODE` as in the snippet below, setting it to `SELWORD`
+   * or whichever name you choose.
+   *
+   *     enum custom_keycodes {
+   *       SELWORD = SAFE_RANGE,
+   *       // Other custom keys...
+   *     };
+   *
+   *     uint16_t SELECT_WORD_KEYCODE = SELWORD;
+   */
+  extern uint16_t SELECT_WORD_KEYCODE;
+
+  /** Handler function for Select Word. */
+  bool process_select_word(uint16_t keycode, keyrecord_t *record);
+
 /**
- * @brief Registers (presses) selection `action`.
+ * @fn select_word_task(void)
+ * Matrix task function for Select Word.
  *
- * The `action` argument in these functions specifies the type of selection:
- *
- *     'W' = word selection
- *     'B' = backward word selection, left of the cursor
- *     'L' = line selection
- *
- * A selection is first registered with `select_word_register(action)`. This
- * should be followed by a call to `select_word_unregister()` to unregister the
- * hotkeys. The point of these separate register and unregister calls is to
- * enable holding the hotkey as a means to extend the selection range.
- *
- * @warning Forgetting to unregister results in stuck keys:
- * `select_word_register()` must be followed by `select_word_unregister()`.
- *
- * @param action  Type of selection to perform.
+ * If using `SELECT_WORD_TIMEOUT`, call this function from your
+ * `housekeeping_task_user()` function in keymap.c. (If no timeout is set,
+ * calling `select_word_task()` has no effect.)
  */
-void select_word_register(char action);
+#if SELECT_WORD_TIMEOUT > 0
+  void select_word_task(void);
+#else
+static inline void select_word_task(void) {}
+#endif // SELECT_WORD_TIMEOUT > 0
 
-/** Unregisters (releases) selection hotkey. */
-void select_word_unregister(void);
+  /**
+   * @brief Registers (presses) selection `action`.
+   *
+   * The `action` argument in these functions specifies the type of selection:
+   *
+   *     'W' = word selection
+   *     'B' = backward word selection, left of the cursor
+   *     'L' = line selection
+   *
+   * A selection is first registered with `select_word_register(action)`. This
+   * should be followed by a call to `select_word_unregister()` to unregister the
+   * hotkeys. The point of these separate register and unregister calls is to
+   * enable holding the hotkey as a means to extend the selection range.
+   *
+   * @warning Forgetting to unregister results in stuck keys:
+   * `select_word_register()` must be followed by `select_word_unregister()`.
+   *
+   * @param action  Type of selection to perform.
+   */
+  void select_word_register(char action);
 
-/** Registers and unregisters ("taps") selection `action.` */
-static inline void select_word_tap(char action) {
-  select_word_register(action);
-  wait_ms(TAP_CODE_DELAY);
-  select_word_unregister();
-}
+  /** Unregisters (releases) selection hotkey. */
+  void select_word_unregister(void);
+
+  /** Registers and unregisters ("taps") selection `action.` */
+  static inline void select_word_tap(char action)
+  {
+    select_word_register(action);
+    wait_ms(TAP_CODE_DELAY);
+    select_word_unregister();
+  }
 
 #if defined(SELECT_WORD_OS_DYNAMIC) || defined(OS_DETECTION_ENABLE)
-/**
- * @brief Callback for whether the host uses Mac vs. Windows/Linux hotkeys.
- *
- * Optionally, this callback may be defined to indicate dynamically whether the
- * keyboard is being used with a Mac or non-Mac system.
- *
- * For instance suppose layer 0 is your base layer for Windows and layer 1 is
- * your base layer for Mac. Indicate this by adding in keymap.c:
- *
- *     bool select_word_host_is_mac(void) {
- *       return IS_LAYER_ON(1);  // Supposing layer 1 = base layer for Mac.
- *     }
- */
-bool select_word_host_is_mac(void);
-#endif  // defined(SELECT_WORD_OS_DYNAMIC) || defined(OS_DETECTION_ENABLE)
+  /**
+   * @brief Callback for whether the host uses Mac vs. Windows/Linux hotkeys.
+   *
+   * Optionally, this callback may be defined to indicate dynamically whether the
+   * keyboard is being used with a Mac or non-Mac system.
+   *
+   * For instance suppose layer 0 is your base layer for Windows and layer 1 is
+   * your base layer for Mac. Indicate this by adding in keymap.c:
+   *
+   *     bool select_word_host_is_mac(void) {
+   *       return IS_LAYER_ON(1);  // Supposing layer 1 = base layer for Mac.
+   *     }
+   */
+  bool select_word_host_is_mac(void);
+#endif // defined(SELECT_WORD_OS_DYNAMIC) || defined(OS_DETECTION_ENABLE)
 
 #ifdef __cplusplus
 }

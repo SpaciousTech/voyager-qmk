6b9e0e9b293ae1029d163a292f5a2551cc24faac
Space <55485515+SpaciousTech@users.noreply.github.com>
2025-04-16 23:08:11 +0200

    adjusting wheel speeds

--- DqN6J/config.h
+++ DqN6J/config.h
@@ -81,13 +81,13 @@
 #undef MOUSEKEY_WHEEL_DELAY
 #define MOUSEKEY_WHEEL_DELAY 0 // Remove delay before scrolling starts (default is 10)
 #undef MOUSEKEY_WHEEL_INTERVAL
-#define MOUSEKEY_WHEEL_INTERVAL 40 // Increase time between scroll steps (default is 80)
+#define MOUSEKEY_WHEEL_INTERVAL 60 // Increase time between scroll steps (default is 80)
 #undef MOUSEKEY_WHEEL_DELTA
 #define MOUSEKEY_WHEEL_DELTA 1 // Increase the amount of scroll steps (default is 1)
 #undef MOUSEKEY_WHEEL_MAX_SPEED
 #define MOUSEKEY_WHEEL_MAX_SPEED 8 // Lower the maximum scroll speed (default is 8)
 #undef MOUSEKEY_WHEEL_TIME_TO_MAX
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 20 // Make it take longer to reach max speed (default is 40)
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40 // Make it take longer to reach max speed (default is 40)
 
 #define SELECT_WORD_OS_MAC
 #define SELECT_WORD_OS_DYNAMIC
